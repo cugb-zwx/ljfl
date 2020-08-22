cc7a120565e163a8013ec0bca811a9d63a614005
zwx228 <956040604@qq.com>
2020-08-22 16:49:02 +0800

    测试类public

--- src/test/java/com/ljfl/LjflApplicationTests.java
+++ src/test/java/com/ljfl/LjflApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LjflApplicationTests {
+public class LjflApplicationTests {
 
     @Test
     void contextLoads() {
